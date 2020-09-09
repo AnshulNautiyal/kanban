6ed3b08ec66945455b1a2da709d9b359879a6c87
AnshulJS <anshul.nautiyal@ril.com>
2020-09-10 01:40:56 +0530

    Updates

---
+++ precache-manifest.5d6c92470e2bca696a4aaca87a54bb3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9516f2c5c7b6fe7a985287eaa3118840",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "b0b897694a0e67b3408c",
+    "url": "/kanban/static/css/main.2b6a3d62.chunk.css"
+  },
+  {
+    "revision": "01206611165e52eb239d",
+    "url": "/kanban/static/js/2.6dca05e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban/static/js/2.6dca05e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b897694a0e67b3408c",
+    "url": "/kanban/static/js/main.03590160.chunk.js"
+  },
+  {
+    "revision": "f5aa037a105c127e43db",
+    "url": "/kanban/static/js/runtime-main.72bc9640.js"
+  }
+]);
